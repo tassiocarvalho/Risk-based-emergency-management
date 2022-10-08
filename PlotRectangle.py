49b10ceb061eb969c24d5e2d2b6454386e5a36fd
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2022-10-08 12:59:23 -0300

    update

--- PlotRectangle.py
+++ PlotRectangle.py
@@ -3,12 +3,18 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
+p1 = int(input("informe o ponto inicial do retângulo X: "))
+p2 = int(input("informe o ponto final do retângulo Y: "))
+larg = int(input("informe a largura do retangulo: : "))
+alt = int(input("informe a altura do retângulo: "))
+ag = input("informe o Valor do ângulo da área: ")
+
 f1 = plt.figure()                       #Plota a figura inicial
 plt.plot([0])                           #Plota a figura que irá ser gerada
 plt.title("Aréa")                       #Titulo da plotagem
-plt.gca().add_patch(Rectangle((0,0),    #paramentro recebe o ponto incial de onde será o retangulo
-                    40,80,              #Largura e altura do retangulo
-                    angle=0,            #define angulo de retangulo
+plt.gca().add_patch(Rectangle((p1,p2),    #paramentro recebe o ponto incial de onde será o retangulo
+                    larg,alt,                  #Largura e altura do retangulo
+                    angle=ag,            #define angulo de retangulo
                     edgecolor='Blue',   #Define cor da borda
                     facecolor='green',  #Define cor da área
                     lw=1))              #Grossura da borda
