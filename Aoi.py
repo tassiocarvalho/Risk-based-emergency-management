d23ea18664c0627e402cb1c34013212f6f9d5bb5
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2022-10-14 12:52:22 -0300

    Class add

---
+++ Aoi.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+class Aoi:                                                  #Classe Área de interesse
+    def __init__(self, x: float, y: float, h: float, w: float, beta: float):
+        self.x = x                       #x
+        self.y = y                       #y
+        self.h = h                       #altura
+        self.w = w                       #comprimento
+        self.beta = beta                 #angulo
+
+    def plot(self):
+        plt.plot([0])                                       #Plota a figura que irá ser gerada
+        plt.title("Aréa")                                   #Titulo da plotagem
+        plt.gca().add_patch(Rectangle((self.x, self.y),     #paramentro recebe o ponto incial de onde será o retangulo
+                            self.w,self.h,                  #Largura e altura do retangulo
+                            angle=self.beta,                #define angulo de retangulo
+                            edgecolor='Blue',               #Define cor da borda
+                            facecolor='none',               #Define cor da área
+                            lw=1))                          #Grossura da borda
+        plt.show()                                          #Mostra a imagem
+##Test##
+area = Aoi(x=0,y= 0,h=40,w=50,beta=20)                      #Instanciando objeto
+area.plot()                                                 #chamando metodo plot para o objeto instaciado
---
+++ CommNet.py
@@ -0,0 +1,6 @@
+#communication network
+class CommNet:
+    def __init__(name, range, accPos):
+        self.name = name
+        self.range = range
+        self.accPos = accPos
