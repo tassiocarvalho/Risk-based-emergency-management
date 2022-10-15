a2c994393d22160a32c7102cbf05b3c90fe11e45
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2022-10-15 16:06:08 -0300

    add objetos

--- Aoi.py
+++ Aoi.py
@@ -1,6 +1,20 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
+class CommNet:
+    def __init__(self, name, range: float, accPosx: float, accPosy: float, color):
+        self.name = name
+        self.range = range
+        self.accPosx = accPosx
+        self.accPosy = accPosy
+        self.color = color
+
+    def plotcircle(self):
+        plt.plot([0]) 
+        plt.title(self.name)
+        circle1 = plt.Circle((self.accPosx, self.accPosy), self.range, color=self.color)
+        plt.gca().add_patch(circle1)
+
 class Aoi:                                                  #Classe Área de interesse
     def __init__(self, x: float, y: float, h: float, w: float, beta: float):
         self.x = x                       #x
@@ -19,6 +33,15 @@ class Aoi:                                                  #Classe Área de int
                             facecolor='none',               #Define cor da área
                             lw=1))                          #Grossura da borda
         plt.show()                                          #Mostra a imagem
-##Test##
-area = Aoi(0,0,40,50,35)                      #Instanciando objeto
-area.plot()                                                 #chamando metodo plot para o objeto instaciado
+
+##TESTS##
+Circle0 = CommNet("circle", 30, 1, 1, "red")                #Instânciando obejeto communication network
+Circle1 = CommNet("circle2", 10, 10, 30, "green")           #Instânciando obejeto communication network
+Circle2 = CommNet("circle3", 20, 30, 20, "pink")            #Instânciando obejeto communication network
+Circle3 = CommNet("cccc", 10, 0, 0, "blue")                 #Instânciando obejeto communication network
+area = Aoi(0,0,30,50,0)                                     #Instânciando obejeto área
+Circle0.plotcircle()                                        #Chamando a função para plotar circulo
+Circle1.plotcircle()                                        #Chamando a função para plotar circulo
+Circle2.plotcircle()                                        #Chamando a função para plotar circulo
+Circle3.plotcircle()                                        #Chamando a função para plotar circulo
+area.plot()                                                 #Chamando a função para plotar a área
