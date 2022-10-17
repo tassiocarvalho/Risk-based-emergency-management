6569269e5fd2316331ffb24f29f8af35dbf5d2c2
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2022-10-17 08:27:56 -0300

    add

--- Aoi.py
+++ Aoi.py
@@ -12,9 +12,9 @@ class CommNet:                                              #Classe communicatio
     def plotcircle(self):
         plt.plot([0]) 
         plt.title(self.name)
-        circle1 = plt.Circle((self.accPosx, self.accPosy), self.range, color=self.color)
+        circle1 = plt.Circle((self.accPosx, self.accPosy), self.range, color=self.color,alpha=.4)
         plt.gca().add_patch(circle1)
-
+        
 class Aoi:                                                  #Classe Área de interesse
     def __init__(self, x: float, y: float, h: float, w: float, beta: float):
         self.x = x                       #x
@@ -37,7 +37,7 @@ class Aoi:                                                  #Classe Área de int
 ##TESTS##
 Circle0 = CommNet("circle", 30, 1, 1, "red")                #Instânciando obejeto communication network
 Circle1 = CommNet("circle2", 10, 10, 30, "green")           #Instânciando obejeto communication network
-Circle2 = CommNet("circle3", 20, 30, 20, "pink")            #Instânciando obejeto communication network
+Circle2 = CommNet("circle3", 20, 30, 20, "brown")            #Instânciando obejeto communication network
 Circle3 = CommNet("cccc", 10, 0, 0, "blue")                 #Instânciando obejeto communication network
 area = Aoi(0,0,30,50,0)                                     #Instânciando obejeto área
 Circle0.plotcircle()                                        #Chamando a função para plotar circulo
